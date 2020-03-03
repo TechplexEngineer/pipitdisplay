51a89e0359802181a1b7c8c7c6e77c014247b44e
Blake Bourque <techplex.engineer@gmail.com>
2020-03-02 19:15:01 -0500

    Starting layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container">
+
+    <div class="row">
+      <div class="col-md-6">Current Event: </div>
+      <div class="col-md-6">Last Updated: </div>
+    </div>
+
+
+
     <div>
       <logo />
       <h1 class="title">
@@ -39,34 +47,7 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+html, body {
+  background: #202020;
 }
 </style>
