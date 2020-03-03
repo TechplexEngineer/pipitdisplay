5d91e504c9f9ec04a7082dfb8a4d9d4d9edc0e66
Blake Bourque <Techplex.Engineer@gmail.com>
2020-03-02 22:33:36 -0500

    beginning async data

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div class="container-fluid px-5 py-4">
 
     <div class="row">
-      <div class="col-8">
-        <h1 class="text-center">Matches</h1>
+      <div class="col">
+        <h2 class="text-center">Matches</h2>
         <table class="table table-striped table-dark table-borderless rounded text-center">
           <thead>
             <tr>
@@ -15,7 +15,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr>
+            <tr v-for="match in matches">
               <th>QM#1</th>
               <td>actual</td>
               <td class="myteam">4909</td>
@@ -26,68 +26,16 @@
               <td>Blue3</td>
               <td>Win 2</td>
             </tr>
-            <tr>
-              <th>QM#2</th>
-              <td>actual</td>
-              <td>Red1</td>
-              <td>Red2</td>
-              <td>Red3</td>
-              <td>Blue1</td>
-              <td class="myteam">4909</td>
-              <td>Blue3</td>
-              <td>Loss 1</td>
-            </tr>
-            <tr>
-              <th>QM#3</th>
-              <td>5:30pm / 6:30pm</td>
-              <td>Red1</td>
-              <td class="myteam">4909</td>
-              <td>Red3</td>
-              <td>Blue1</td>
-              <td>Blue2</td>
-              <td>Blue3</td>
-              <td></td>
-            </tr>
+            
           </tbody>
         </table>
       </div>
-
-      <div class="col">
-        <h1 class="text-center display-5">Next Match <small class="smaller">QM#5</small></h1>
-
-        <table class="table table-striped table-dark table-borderless rounded shadow-sm">
-          <thead class="text-center thead-dark">
-            <tr class="rounded-top">
-              <th class="font-weight-bold bg-danger">Red</th>
-              <th class="font-weight-bold bg-primary">Blue</th>
-            </tr>
-          </thead>
-          <tbody class="text-center rounded-bottom">
-            <tr>
-              <td>Red1</td>
-              <td>Blue1</td>
-            </tr>
-            <tr>
-              <td>Red2</td>
-              <td class="myteam">4909</td>
-            </tr>
-            <tr class="rounded-bottom">
-              <td>Red3</td>
-              <td>Blue3</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <div class="row text-center">
-          <div class="col">Scheduled: 05:30 PM</div>
-          <div class="col">Predicted: 05:45 PM</div>
-        </div>
-
-      </div>
-
     </div>
 
+      
+
     <div class="row pt-4">
+      <div class="col-2"></div>
       <div class="col-4">
         <h2 class="text-center">Rankings <small class="smaller">Rank 1 of 35</small></h2>
 
@@ -134,11 +82,43 @@
           </tbody>
         </table>
       </div>
-      <div class="col">
-        <h2 class="text-center">Countdown</h2>
+      <div class="col-4">
+        <h2 class="text-center display-5">Next Match <small class="smaller">QM#5</small></h2>
+
+        <table class="table table-striped table-dark table-borderless rounded shadow-sm">
+          <thead class="text-center thead-dark">
+            <tr class="rounded-top">
+              <th class="font-weight-bold bg-danger">Red</th>
+              <th class="font-weight-bold bg-primary">Blue</th>
+            </tr>
+          </thead>
+          <tbody class="text-center rounded-bottom">
+            <tr>
+              <td>Red1</td>
+              <td>Blue1</td>
+            </tr>
+            <tr>
+              <td>Red2</td>
+              <td class="myteam">4909</td>
+            </tr>
+            <tr class="rounded-bottom">
+              <td>Red3</td>
+              <td>Blue3</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div class="row text-center">
+          <div class="col">Scheduled: 05:30 PM</div>
+          <div class="col">Predicted: 05:45 PM</div>
+        </div>
+
+        <h2 class="text-center">1d 5h 53m</h2>
       </div>
+
     </div>
 
+    <!-- Data Input -->
     <div class="row text-muted">
       <div class="col-sm text-center">
         Team: <input type="text" name="team"/>
@@ -155,13 +135,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  asyncData ({ params }) {
+    return {matches: [1,2,3,4,5,6,7,8,9,10,11,12]}
   }
+
 }
+
 </script>
 
 <style scoped>
@@ -171,7 +151,7 @@ export default {
 .rounded-bottom-right { border-bottom-right-radius: .25rem !important; }
 
 .smaller {
-  font-size: 45%;
+  font-size: 70%;
 }
 
 .myteam {
